9c63143b6c45d37cbaa1d6644495a5102c709e47
VaritoTorres <varito.torrezmtz@gmail.com>
2026-05-25 19:30:42 -0600

    add routing for carrito, catalogo, and principal components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Carrito } from './carrito/carrito';
+import { Catalogo } from './catalogo/catalogo';
+import { Principal } from './principal/principal';
+
+export const routes: Routes = [
+  { path: 'principal', component: Principal },
+  { path: 'catalogo', component: Catalogo },
+  { path: 'carrito', component: Carrito },
+
+];
