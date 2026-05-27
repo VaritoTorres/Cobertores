2e05ee8c93aea630125c1e66aa01d6ece92ae9cf
Carolina <carolinaleonmad@gmail.com>
2026-05-27 12:36:16 -0600

    add administracion-basica component with HTML and CSS files

---
+++ src/app/administracion-basica/administracion-basica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administracion-basica',
+  imports: [],
+  templateUrl: './administracion-basica.component.html',
+  styleUrl: './administracion-basica.component.css'
+})
+export class AdministracionBasicaComponent {
+
+}
