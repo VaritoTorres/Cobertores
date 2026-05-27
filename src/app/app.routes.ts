12dd723e9e0b444e8e5a01705e50d4f68242f563
Misael pro <misaelpablo388@gmail.com>
2026-05-27 13:14:45 -0600

    feat: add carrito, catalogo, and principal components with templates and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './home/home';
 import { Productos } from './productos/productos';
+
 import { Carrito } from './carrito/carrito';
 import { Catalogo } from './catalogo/catalogo';
 import { Principal } from './principal/principal';
-
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -12,6 +13,5 @@ export const routes: Routes = [
 
   { path: 'principal', component: Principal },
   { path: 'catalogo', component: Catalogo },
-  { path: 'carrito', component: Carrito }
   { path: 'carrito', component: Carrito }
 ];
