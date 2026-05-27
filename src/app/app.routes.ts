936ba03ee18689a02a536e6e1614871a6e97b74b
VaritoTorres <varito.torrezmtz@gmail.com>
2026-05-27 12:47:52 -0600

    implement menu and carrito components with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-
 import { Carrito } from './carrito/carrito';
 import { Catalogo } from './catalogo/catalogo';
 import { Principal } from './principal/principal';
+
 
 export const routes: Routes = [
   { path: 'principal', component: Principal },
   { path: 'catalogo', component: Catalogo },
   { path: 'carrito', component: Carrito },
-
 ];
