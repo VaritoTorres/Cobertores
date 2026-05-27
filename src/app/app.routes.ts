2b97f66cb8e045b5a020638c618f7c688d1e6c4b
VaritoTorres <varito.torrezmtz@gmail.com>
2026-05-27 13:55:50 -0600

    fix: update default route to Login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
+import { Login } from './login/login';
 import { Home } from './home/home';
 import { Productos } from './productos/productos';
 
@@ -8,9 +9,10 @@ import { Catalogo } from './catalogo/catalogo';
 import { Principal } from './principal/principal';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', component: Login },
+  { path: 'home', component: Home },
   { path: 'productos', component: Productos },
   { path: 'principal', component: Principal },
   { path: 'catalogo', component: Catalogo },
   { path: 'carrito', component: Carrito }
-];
+];  
